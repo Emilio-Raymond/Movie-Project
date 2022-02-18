eb2af125bd888431f3ac3a348b73c7668bb3861b
Emilio Benavides <emilio.benavides68@gmail.com>
2022-02-18 16:47:47 -0600

    added max height for img poster, added JS to produce movie list

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,30 @@
 const BASE = 'https://windy-brawny-lumber.glitch.me/movies'
 $.get(BASE, function () {
     console.log('loading')
-}).done(function (results){
+}).done(function (results) {
     console.log(results)
+    results.forEach(function (movie) {
+        const HTML = creatingHtml(movie.poster, movie.title, movie.year, 'war' , movie.genre, movie.plot)
+        $(`#movie-insert`).append(HTML)
+    })
 })
+
+function creatingHtml(imgSrc, title, yearRel, runTime, genre, plotDes) {
+    //language=HTML
+    return `
+        <div id="movie" class="movie-container">
+            <div id="img-div">
+                <img src="${imgSrc}"
+                     alt="Movie Poster">
+            </div>
+            <div id="movie-info">
+                <p>${title}</p>
+                <p>${yearRel}</p>
+                <p>${runTime}</p>
+                <p>${genre}</p>
+            </div>
+            <div id="plot">
+                <p>${plotDes}</p>
+            </div>
+        </div>`
+}
