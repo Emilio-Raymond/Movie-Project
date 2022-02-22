425412f76e99c7339396afa762085fc916a7e823
Emilio Benavides <emilio.benavides68@gmail.com>
2022-02-22 11:18:37 -0600

    created deconstruction of array to allow correct value, added hidden class for mogule.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,4 @@
 "use strict;"
-const BASE = 'https://windy-brawny-lumber.glitch.me/movies'
-
-function getAllMovies() {
-    $.get(BASE, function () {
-        $(`#movie-insert`).html('Get Your POPCORN READY!')
-    }).done(function (results) {
-        console.log(results)
-        $(`#movie-insert`).html('')
-        results.forEach(function (movie) {
-            //     if (movie.title === undefined) {
-            //
-            //         deleteMovie(movie.id)
-            //     }
-            const HTML = creatingHtml(movie.poster, movie.title, movie.year, 'war', movie.genre, movie.plot)
-            $(`#movie-insert`).append(HTML)
-        })
-    })
-}
-
-getAllMovies()
-
-
 function creatingHtml(imgSrc, title, yearRel, runTime, genre, plotDes) {
     //language=HTML
     return `
@@ -30,43 +8,25 @@ function creatingHtml(imgSrc, title, yearRel, runTime, genre, plotDes) {
                      alt="Movie Poster">
             </div>
             <div id="movie-info">
-                <p>${title}</p>
-                <p>${yearRel}</p>
-                <p>${runTime}</p>
-                <p>${genre}</p>
+                <p>Title: ${title}</p>
+                <p>Year Released: ${yearRel}</p>
+                <p>Runtime: ${runTime}</p>
+                <p>Genre: ${genre}</p>
             </div>
             <div id="plot">
+                <p>Plot: </p>
                 <p>${plotDes}</p>
             </div>
         </div>`
 }
 
-function deleteMovie(id) {
-    $.ajax({url: `${BASE}/${id}`, type: 'DELETE',}).done(function (data) {
-        console.log("deletesuccess")
-    })
-}
-
 function createMovie() {
-    const [title, year, genre, director, rating] = $('form').children()
-    return {
-        title: title.value,
-        yearReleased: year.value,
-        genre: genre.value,
-        director: director.value,
-        rating: rating.value,
-    }
-
-}
-
-function addMovie(newMovie) {
-    console.log(newMovie);
-    $.post(BASE, newMovie).done(function () {
-        getAllMovies()
-    })
+    const [title] = $('form').children()
+    return title.value
 }
 
 $('#createMovie').click(function (e) {
     e.preventDefault();
-    addMovie(createMovie())
+    //addMovie(createMovie())
+   getMovieData(createMovie())
 })
